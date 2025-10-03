6a898cf8006102e86a88640cff39fd3bfc730dcc
Naveed Ahmed <jaannaveed827@gmail.com>
2025-10-03 20:11:13 +0500

    Fixed import paths and updated CSS/UI

---
+++ src/components/Stats.js
@@ -0,0 +1,24 @@
+function Stats({ items }) {
+  if (items.length === 0)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You are ready to go! ✈️"
+          : `💼 You have ${numItems} items on your list, and you are bringing 🧳 ${numPacked} packed. (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
+
+export default Stats;
